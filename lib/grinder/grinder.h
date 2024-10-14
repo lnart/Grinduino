b9f54768bc514eeea6b66a1bdaacb8697cfc18e7
lnart <lennart.pafel@code.berlin>
2024-10-14 12:46:21 +0200

    define grinder class

---
+++ lib/grinder/grinder.h
@@ -0,0 +1,32 @@
+#ifndef grinder.h
+#define grinder.h
+
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <button.h>
+#include <motor.h>
+
+class GrinderClass {
+    public:
+        GrinderClass(int in1Pin, int in2Pin, int enaPin, int speedPin, int durationPin, int startPin);
+        void begin();
+        void update();
+        void updateLCD();
+
+    private:
+        ButtonClass _startButton;
+        MotorClass _motor;
+        LiquidCrystal _lcd;
+
+        int _speedPin, _durationPin;
+        bool _motorRunning;
+        int _speedReading, _speed, _readableSpeed;
+        int _durationReading, _duration;
+        unsigned long _motorStartTime;
+        float _remainingTime;
+
+        int smoothAnalogRead(int pin, int samples = 10);
+
+};
+
+#endif
