c9d2440eccd6ef5a1671e2038229d070cb18a2b2
lnart <lennart.pafel@code.berlin>
2024-10-14 12:46:48 +0200

    implement main file

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <grinder.h>
+
+
+const int IN1 = 9;
+const int IN2 = 8;
+const int ENA = 10;
+const int SPEED_PIN = A0;
+const int DURATION_PIN = A1;
+const int START_PIN = 6;
+
+GrinderClass coffeeGrinder(IN1, IN2, ENA, SPEED_PIN, DURATION_PIN, START_PIN);
+
+void setup() {
+    Serial.begin(9600);
+    coffeeGrinder.begin();
+    Serial.println("Coffee Grinder initialized");
+
+}
+
+void loop() {
+    coffeeGrinder.update();
+    coffeeGrinder.updateLCD();
+
+    delay(100);
+}
