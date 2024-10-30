0ff52ca10ac6a15278c8c5b01e1aa08d229e9bbd
lnart <lennart.pafel@code.berlin>
2024-10-30 10:03:16 +0100

    change config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: 'img/Coffee-Brew-Icon.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://lnart.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/grinduino/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lnart', // Usually your GitHub org/user name.
   projectName: 'grinduino', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
