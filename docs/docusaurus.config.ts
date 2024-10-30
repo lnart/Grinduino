5fe7fe7b7d518c7da08fc223b4ec1db00f872c59
lnart <lennart.pafel@code.berlin>
2024-10-30 11:54:33 +0100

    cgange baseurl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://lnart.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/grinduino/',
+  baseUrl: '/Grinduino/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
